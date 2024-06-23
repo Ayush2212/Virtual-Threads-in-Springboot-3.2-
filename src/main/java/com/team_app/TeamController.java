e32bbd0c4b4dc1e9e78d2a613bb4d4f39ec226ec
Ayush <you@example.com>
2024-06-23 23:56:15 +0530

    server on which multiple concurrent requests are hit

---
+++ src/main/java/com/team_app/TeamController.java
@@ -0,0 +1,29 @@
+package com.team_app;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@RestController
+@Log4j2
+public class TeamController {
+
+    private RestClient restClient;
+
+    public  TeamController(RestClient.Builder builder){
+        this.restClient=builder.baseUrl("http://localhost:8081").build();
+    }
+    @GetMapping("/")
+    public List<String> getPlayersForTeam(){
+        log.info("Running on {} ", Thread.currentThread());
+            return restClient.get().
+                    uri("/players").
+                    retrieve()
+                    .toEntity(List.class)
+                    .getBody();
+
+    }
+}
